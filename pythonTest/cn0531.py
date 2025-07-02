2a61c8a9823586f90a8e9d609892ed8c7f7c6573
scwook <scwook@ibs.re.kr>
2025-07-03 07:49:46 +0900

    MR

--- pythonTest/cn0531.py
+++ pythonTest/cn0531.py
@@ -13,36 +13,39 @@ try:
 #    resp = spi.xfer2(tx)
 #    time.sleep(0.5)
 
+    # Set control register
     tx = [0x20, 0x00, 0x10]
     print(f"Sending: {[hex(x) for x in tx]}")
     resp = spi.xfer2(tx)
 #    time.sleep(0.5)
 
-    tx = [0x1f, 0xff, 0xff]
+    # Set DAC max output voltage
+    tx = [0x1f, 0x00, 0x00]
     print(f"Sending: {[hex(x) for x in tx]}")
     resp = spi.xfer2(tx)
 #    time.sleep(0.5)
 
-    tx = [0x40, 0x00, 0x00]
-    print(f"Sending: {[hex(x) for x in tx]}")
-    resp = spi.xfer2(tx)
+    # Set software register Reset 0, CLR 0, LDAC 0
+#    tx = [0x40, 0x00, 0x00]
+#    print(f"Sending: {[hex(x) for x in tx]}")
+#    resp = spi.xfer2(tx)
 #    time.sleep(5)
 
-
-    tx = [0x40, 0x00, 0x40]
-    print(f"Sending: {[hex(x) for x in tx]}")
-    resp = spi.xfer2(tx)
+    # Set software register : Reset 1, CLR 0, LDAC 0
+#    tx = [0x40, 0x00, 0x40]
+#    print(f"Sending: {[hex(x) for x in tx]}")
+#    resp = spi.xfer2(tx)
 #    time.sleep(0.5)
 
-    rx_dac = [0x90, 0x00, 0x00]
-    rx_ctrl = [0xa0, 0x00, 0x00]
-    rx_clr = [0xb0, 0x00, 0x00]
+#    rx_dac = [0x90, 0x00, 0x00]
+#    rx_ctrl = [0xa0, 0x00, 0x00]
+#    rx_clr = [0xb0, 0x00, 0x00]
 
 
-    rev = spi.xfer2(rx_clr)
-    time.sleep(0.5)
+#    rev = spi.xfer2(rx_clr)
+#    time.sleep(0.5)
 
-    print(f"Received: {[hex(x) for x in rev]}")
+#    print(f"Received: {[hex(x) for x in rev]}")
 
 
 except Exception as e:
@@ -51,3 +54,5 @@ except Exception as e:
 finally:
     spi.close()
     print("SPI port closed.")
+
+
