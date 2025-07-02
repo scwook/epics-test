5fd7ed78a7dfcc77af808e74f4c53ef047c12fa4
scwook <scwook@ibs.re.kr>
2025-07-03 07:24:42 +0900

    add cn0531

--- README.md
+++ README.md
@@ -10,3 +10,4 @@ EPICS 개발용 테스트 저장소
 ### pythonTest
 - opiMaker.py : text update widget을 원하는 개수만큼 만드는 코드
 - oriental_ard-kd_checksum.py : 오리엔탈 모터 드라이버 ARD-KD Error check 값 계산하는 코드
+- cn0531.py : Analog Device 제품 CN0531 보드 Analog output 테스트용 코드 (Raspberry Pi)
---
+++ pythonTest/cn0531.py
@@ -0,0 +1,53 @@
+import spidev
+import time
+
+spi = spidev.SpiDev()
+spi.open(0,0)
+
+spi.max_speed_hz = 1000000
+spi.mode = 1
+
+try:
+#    tx = [0x40, 0x00, 0x04]
+#    print(f"Sending: {[hex(x) for x in tx]}")
+#    resp = spi.xfer2(tx)
+#    time.sleep(0.5)
+
+    tx = [0x20, 0x00, 0x10]
+    print(f"Sending: {[hex(x) for x in tx]}")
+    resp = spi.xfer2(tx)
+#    time.sleep(0.5)
+
+    tx = [0x1f, 0xff, 0xff]
+    print(f"Sending: {[hex(x) for x in tx]}")
+    resp = spi.xfer2(tx)
+#    time.sleep(0.5)
+
+    tx = [0x40, 0x00, 0x00]
+    print(f"Sending: {[hex(x) for x in tx]}")
+    resp = spi.xfer2(tx)
+#    time.sleep(5)
+
+
+    tx = [0x40, 0x00, 0x40]
+    print(f"Sending: {[hex(x) for x in tx]}")
+    resp = spi.xfer2(tx)
+#    time.sleep(0.5)
+
+    rx_dac = [0x90, 0x00, 0x00]
+    rx_ctrl = [0xa0, 0x00, 0x00]
+    rx_clr = [0xb0, 0x00, 0x00]
+
+
+    rev = spi.xfer2(rx_clr)
+    time.sleep(0.5)
+
+    print(f"Received: {[hex(x) for x in rev]}")
+
+
+except Exception as e:
+    print(f"An error occurred: {e}")
+
+finally:
+    spi.close()
+    print("SPI port closed.")
